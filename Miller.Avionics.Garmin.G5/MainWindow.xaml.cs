419d6797cf032daca2606a720d66795d4e82d722
Andrew Miller <andywmm9@gmail.com>
2019-02-21 17:49:54 -0600

    Added attitude fail UserControl and renamed MainWindow.

--- Miller.Avionics.Garmin.G5/MainWindow.xaml.cs
+++ Miller.Avionics.Garmin.G5/MainWindow.xaml.cs
@@ -5,9 +5,9 @@ namespace Miller.Avionics.Garmin.G5
     /// <summary>
     /// Interaction logic for MainWindowTest.xaml
     /// </summary>
-    public partial class MainWindowTest : Window
+    public partial class MainWindow : Window
     {
-        public MainWindowTest()
+        public MainWindow()
         {
             InitializeComponent();
         }
